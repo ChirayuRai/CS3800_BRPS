0945324b35eb2a20e6cca8bec8522386296f8fc4
Edwin <edwin.orjales@gmail.com>
2022-11-16 08:37:46 -0800

    Update lobby and unversion buffers

--- index.php
+++ index.php
@@ -16,6 +16,9 @@ $numRooms = 5;
 		.gameroom h2 {
 			margin: 0;
 		}
+		.gameroom button {
+			margin-right: 15px;
+		}
 		.g1 {
 			background-color: darkgreen;
 		}
@@ -36,6 +39,8 @@ $numRooms = 5;
 <body>
 	<?php
 		for ($i=1; $i <= $numRooms; $i++) { 
+			$available = array(true, true);
+			$room = 42068 + $i;
 			?>
 			<div class="gameroom g<?=$i;?>">
 				<h2>Room <?=$i;?></h2>
@@ -48,25 +53,29 @@ $numRooms = 5;
 							foreach ($json_data as $row) {
 								$event = json_decode($row, true);
 								if($event["event"] == "login_ok" && $event["context"] == "1") {
-									$result = "Room available for P2 to join";
-									$players++;
+									$available[0] = false;
 								}
 								elseif($event["event"] == "login_ok" && $event["context"] == "2") {
+									$available[1] = false;
 									$result = "Room available for P1 to join";
-									$players++;
 								} elseif($players >= 2) {
-									$result = "Room Unavailable";
 									break;
 								}
 							}
-							echo $result;
-						} else {
-							echo 'Room Available for P1 and P2 to join';
 						}
 					?>
 				</p>
-				<button type="button" onclick="location.href='http://cpp3800.edwin-dev.com/gameroom.php?room<?=42068+$i?>&p=1'">Join as P1</button>
-				<button type="button" onclick="location.href='http://cpp3800.edwin-dev.com/gameroom.php?room<?=42068+$i?>&p=2'">Join as P2</button>
+				<?php
+					if($available[0]) {
+						echo "<button type='button' onclick='location.href=\"http://cpp3800.edwin-dev.com/gameroom.php?room={$room}&p=1\"'>Join as P1</button>";
+					}
+					if($available[1]) {
+						echo "<button type='button' onclick='location.href=\"http://cpp3800.edwin-dev.com/gameroom.php?room={$room}&p=1\"'>Join as P2</button>";
+					}
+					if(!$available[0] && !$available[1]) {
+						echo "Room is occupied";
+					}
+				?>
 			</div>
 			<?php
 		}
